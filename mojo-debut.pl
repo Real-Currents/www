2b80a414d0dbf93566af09a4d290d02743685ff5
John <john@real-currents.com>
2015-03-17 15:09:57 -0700

    log mojolicious version

--- mojo-debut.pl
+++ mojo-debut.pl
@@ -5,7 +5,6 @@ use warnings;
 
 # Start command line interface for application
 use Mojolicious::Commands;
-say Mojolicious->VERSION;
 BEGIN { unshift( @INC, "lib"); }
 
 Mojolicious::Commands->start_app('Mojo::Debut');
