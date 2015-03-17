b108d2727c58a5f1c5294fcd90cb2548e96d86f7
John <john@real-currents.com>
2015-03-17 14:24:36 -0700

    add Perloku and other stuff

--- mojo-debut.pl
+++ mojo-debut.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/env perl
+use 5.10.1;
 use strict;
 use warnings;
 
 # Start command line interface for application
 use Mojolicious::Commands;
+say Mojolicious->VERSION;
 BEGIN { unshift( @INC, "lib"); }
 
 Mojolicious::Commands->start_app('Mojo::Debut');
