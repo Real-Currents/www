fe8a93aafebe5e8dded8c7f66bd32083efc4dd8d
John <john@real-currents.com>
2015-04-21 07:22:16 -0700

    reconfigure for contenticious

--- mojo-debut.pl
+++ mojo-debut.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
-use 5.10.1;
-use strict;
 use warnings;
+use strict;
+use :5.10;
 
 # Start command line interface for application
 BEGIN { unshift( @INC, "lib"); }
+use Contenticious;
+use Contenticious::Commands;
 use Mojolicious::Commands;
 
 Mojolicious::Commands->start_app('Mojo::Debut');
