e9d6825aec1c6acad956a38ad6921a6c1e6b20ae
John <john@real-currents.com>
2020-07-13 06:00:46 -0400

    add sveltr

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy'
 import livereload from 'rollup-plugin-livereload';
+import resolve from 'rollup-plugin-node-resolve';
+import postcss from "rollup-plugin-postcss";
+import shader from 'rollup-plugin-shader';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -12,7 +15,8 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		exports: 'named',
+		file: 'public/main.js'
 	},
 	plugins: [
 		svelte({
@@ -21,7 +25,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write('public/main.css');
 			}
 		}),
 
@@ -34,7 +38,32 @@ export default {
 			browser: true,
 			dedupe: ['svelte']
 		}),
+
+		postcss({
+			extract: 'public/global.css',
+			plugins: []
+		}),
+
 		commonjs(),
+
+		copy({
+			targets: [
+				{ src: 'src/images', dest: 'public/' },
+				{ src: 'static/*', dest: 'public/' }
+			]
+		}),
+
+		shader( {
+			// All match files will be parsed by default,
+			// but you can also specifically include/exclude files
+			include: [
+				'../@sveltejs/gl/**/*.glsl',
+				'**/*.glsl',
+				'**/*.vs',
+				'**/*.fs' ],
+			// specify whether to remove comments
+			removeComments: true,   // default: true
+		} ),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
