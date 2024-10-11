ed57b9fe71b19c9ef4217ad5dab5484bd922a709
dancingfrog <john@real-currents.com>
2024-10-11 07:11:56 -0400

    add link to dev profile

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,13 @@ export default function Home() {
     const introMatter = getIntroContent();
     const postMetadata: Post[] = // await fetch('https://.../posts').then((res) => res.json())
         [
-            { title: "Apple Pie", slug: "apple_pie" }, // => /content/apple_pie.html
-            { title: "Banana Bread", slug: "banana_bread" }, // => /content/banana_bread.html
-            { title: "Blueberry Scones", slug: "blueberry_scones" }, // => /content/blueberry_scones.html
-            { title: "Chocolate Chip Cookies", slug: "chocolate_chip_cookies" }, // => /content/chocolate_chip_cookies.html
-            { title: "Cinnamon Rolls", slug: "cinnamon_rolls" }, // => /content/cinnamon_rolls.html
-            { title: "Lemon Poppy Seed Muffins", slug: "lemon_poppy_seed_muffins" } // => /content/lemon_poppy_seed_muffins.html
+            // { title: "Apple Pie", slug: "apple_pie" }, // => /content/apple_pie.html
+            // { title: "Banana Bread", slug: "banana_bread" }, // => /content/banana_bread.html
+            // { title: "Blueberry Scones", slug: "blueberry_scones" }, // => /content/blueberry_scones.html
+            // { title: "Chocolate Chip Cookies", slug: "chocolate_chip_cookies" }, // => /content/chocolate_chip_cookies.html
+            // { title: "Cinnamon Rolls", slug: "cinnamon_rolls" }, // => /content/cinnamon_rolls.html
+            // { title: "Lemon Poppy Seed Muffins", slug: "lemon_poppy_seed_muffins" } // => /content/lemon_poppy_seed_muffins.html
+            { title: "Meet the Developer", slug: "README" }
         ];
 
     console.log(postMetadata);
@@ -34,7 +35,8 @@ export default function Home() {
             <div className={"postContainer"}>
                 {postMetadata.map((post, index) => {
                     return (
-                        <PostCard contentRoot={"content/recipes"}
+                        // <PostCard contentRoot={"content/recipes"}
+                        <PostCard contentRoot={"content/dev"}
                                     key={"post-" + index}
                                     post={(post as Post)}>
                         </PostCard>
